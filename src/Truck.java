1d83d8c2d563c74b66bcbd0222fec16fe0a36eba
Staol <kolontayev1@gmail.com>
2020-11-28 18:19:34 +0100

    Removed ramp : Ramp from Truck. The Ramp has association with each of Truck's subclasses

--- src/Truck.java
+++ src/Truck.java
@@ -3,7 +3,6 @@ import java.awt.*;
 /** Represents a base model of a truck that a truck is based on.
  */
 public abstract class Truck extends Vehicle {
-    private Ramp ramp = new Ramp();
 
     public Truck(double x, double y, int nrDoors, double enginePower, double currentSpeed,
                  Color color, String modelName, int directionAngle) {
