842d3415ba3fb140ad990f26fb1f3584e0750a79
Staol <kolontayev1@gmail.com>
2020-11-28 18:16:02 +0100

    Added Javadoc for Ramp

--- src/Ramp.java
+++ src/Ramp.java
@@ -1,17 +1,27 @@
 public class Ramp {
     private boolean rampUp = true;
 
-    public boolean isRampRaised() {
-        return rampUp;
-    }
-
+    /**
+     * Sets ramp up
+     */
     public void raiseRamp() {
         rampUp = true;
 
     }
 
+    /**
+     * Sets the ramp down
+     */
     public void lowerRamp() {
         rampUp = false;
+    }
+
+    /**
+     * Checks if ramp is up
+     * @return a boolean representing if ramp is up or down
+     */
+    public boolean isRampRaised() {
+        return rampUp;
     }
 
 
