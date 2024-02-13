ef2d73dbbb9b6c7bef3149f6c2ddf8812b80566a
Elina Norling <elinano@chalmers.se>
2024-02-13 16:01:33 +0100

    felmeddelanden lösta

--- Scania.java
+++ Scania.java
@@ -7,14 +7,16 @@ public class Scania extends Truck{
         super(nrDoors, enginePower, color, modelName);
     }
 
-    @Override
-    public void move(){
-        System.out.println("Moving truck with bed angle: " + scaniaBed.getAngle() + ", speed: " + getCurrentSpeed());
-        if (scaniaBed.getAngle() == 0)
-            super.move();
-        else
-            throw new IllegalStateException("Can't move when bed is raised");
-    }
+//    @Override
+//    public void move(){ //vad är skillanden mot gas? varför behövs båda? vi får väl move från vehicle?
+//        //den här körs hela tiden när programmet startas... Antagligen därför man inte kna raisa bed direkt pga datorn tror att scania redan rör sig
+//        //System.out.println("Moving truck with bed angle: " + scaniaBed.getAngle() + ", speed: " + getCurrentSpeed());
+//        if (scaniaBed.getAngle() == 0)
+//            super.move();
+//        else
+//            throw new IllegalStateException("Can't move when bed is raised");
+//    }
+//    //en fråga: ska alla bilar inte kunna köra om bed är raised? nej väl?
 
     protected int getBedAngle() {
         return scaniaBed.getAngle();
